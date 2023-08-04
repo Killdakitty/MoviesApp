e5cf854bbba52e8f97e223cdf8858b10d4d894cd
Alex <cheny737@gmail.com>
2023-08-04 11:57:19 -0400

    commit2

--- src/App.js
+++ src/App.js
@@ -2,19 +2,49 @@ import {useState, useEffect} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 // WE IMPORT OUR COMPONENTS
-import MovieDisplay from "./components/MovieDisplay";
+import MovieDisplay from "./components/MovieDisney";
 import Form from "./components/Form";
 
+// console(process.env.REACT_APP_MOVIE_API_KEY);
 
-function App() {
+export default function App() {
+  const apiKey="ac1eaee6Y";
+
+  //state to hold movie data
+  const [movie, setMovie]= useState (null);
+  //Function to getMovies
+  const getMovie = async (searchTerm) => {
+    // make fetch request and store response
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}&t=${searchTerm}`
+      );
+      // Parse JSON response into a javascript object
+      const data = await response.json();
+      //set the Movie state to the movie
+      setMovie(data);
+    } catch(e){
+      console.error(e)
+    }
+  };
+
+    //This will run on the first render but not on subsquent renders
+    useEffect(() => {
+      getMovie("Clueless");
+    }, []);
+
+    
+  //USE OUR COMPONENTS IN APPs RETURNED JSX
+  // We pass
   return (
+
+    
     <div className="App">
       
-      <Form />
-      <MovieDisplay />
+      <Form moviesearch={getMovie}/>
+      <MovieDisplay movie={movie} />
       
     </div>
   );
 }
 
-export default App;
